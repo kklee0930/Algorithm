df57151b8f5361a06a9a6c7757d703a99a77bb80
Kevin Lee <kklee0930@gmail.com>
2022-07-20 18:33:32 +0900

    Delete SWEA directory

--- SWEA/1933.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-num_list = []
-for i in range(1, N+1):
-    if N % i == 0:
-        num_list.append(i)
-num_list.sort()
-for i in num_list:
-    print(i, end=' ')
--- SWEA/2050.py
+++
@@ -1,7 +0,0 @@
-input_str = str(input())
-l = []
-for i in input_str:
-    i = ord(i) - 64
-    l.append(i)
-for i in l:
-    print(i, end=' ')
