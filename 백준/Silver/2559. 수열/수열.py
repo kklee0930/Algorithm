a0df55f5324ae3e702dcf0f136c45ff5e8cbb724
Kevin Lee <kklee0930@gmail.com>
2022-08-16 01:46:08 +0900

    [Silver III] Title: 수열, Time: 112 ms, Memory: 39572 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,9 +1,6 @@
 N, K = map(int, input().split()) # 온도 측정한 전체 날짜의 수, 연속적인 K일
 temp_lst = list(map(int, input().split())) # 측정한 온도의 수열
 
-answer = 0 
-start = 0
-
 test = [sum(temp_lst[:K])]
 
 for i in range(len(temp_lst)-K):
