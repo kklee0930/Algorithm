acff8fe7d473c68822a66799aa2397a6f34edea4
Kevin Lee <kklee0930@gmail.com>
2023-04-18 23:34:06 +0900

    [level 2] Title: 더 맵게, Time: 1631.24 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -4,6 +4,8 @@ def solution(scoville, K):
     
     cnt = 0
     while True:
+        if len(scoville) <= 1 and scoville[0] < K: # 힙에 원소가 하나 남을 경우 더 이상 진행이 불가능하므로 -1
+            return -1
         min_ = heapq.heappop(scoville) # 가장 맵지 않은 음식의 스코빌 지수
         if min_ >= K: # K 이상이면 중지
             break
@@ -11,6 +13,4 @@ def solution(scoville, K):
         min_2 = heapq.heappop(scoville)
         heapq.heappush(scoville, min_ + (min_2*2))
         
-        if len(scoville) <= 1 and scoville[0] < K: # 힙에 원소가 하나 남을 경우 더 이상 진행이 불가능하므로 -1
-            return -1
     return cnt
