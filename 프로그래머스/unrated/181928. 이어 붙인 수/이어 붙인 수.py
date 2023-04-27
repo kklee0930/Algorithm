ef2abaf1abad4e8b095663a18323f823834b5ce0
Kevin Lee <kklee0930@gmail.com>
2023-04-27 16:36:25 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    odd = ''
+    even = ''
+    for n in num_list:
+        if n % 2 == 0:
+            even += str(n)
+        else:
+            odd += str(n)
+    return int(even) + int(odd)
