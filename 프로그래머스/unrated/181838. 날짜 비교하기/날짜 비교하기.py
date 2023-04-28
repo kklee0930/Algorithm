53fa8c6dedaa765a7f88d16c0b1fc0281a446cc3
Kevin Lee <kklee0930@gmail.com>
2023-04-29 00:58:35 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,7 @@
+def solution(date1, date2):
+    days1 = date1[0] * 365 + date1[1] * 30 + date1[2]
+    days2 = date2[0] * 365 + date2[1] * 30 + date2[2]
+    if date1 < date2:
+        return 1
+    else:
+        return 0
