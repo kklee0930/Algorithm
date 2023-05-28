32e30d89aa2728b32c12d67d79b3e1588bbd4406
Kevin Lee <kklee0930@gmail.com>
2023-05-28 21:39:42 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        System.out.println(Integer.parseInt(input[0]) - Integer.parseInt(input[1]));
+    }
+}
