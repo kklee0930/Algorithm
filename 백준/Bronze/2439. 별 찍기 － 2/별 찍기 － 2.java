bf6b106e09225dd74c708c095da686d316aced39
Kevin Lee <kklee0930@gmail.com>
2023-05-30 21:07:03 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 372 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        for(int i = 1; i <= tc; i++){
+            for(int k = 1; k<= tc-i; k++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j ++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
