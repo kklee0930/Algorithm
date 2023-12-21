cb21923d7bcf910c8c077f9e9215a00adcd08d0b
Kevin Lee <kklee0930@gmail.com>
2023-12-21 23:32:43 +0900

    [Bronze V] Title: N 찍기, Time: 1160 ms, Memory: 34212 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        for(int i = 1; i < number + 1; i++) {
+            System.out.println(i);
+        }
+    }
+}
