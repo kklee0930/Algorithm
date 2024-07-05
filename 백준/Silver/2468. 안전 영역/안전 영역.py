7187128750e93469b4325985f967888bb366df13
Kevin Lee <kklee0930@gmail.com>
2024-07-06 02:46:24 +0900

    [Silver I] Title: 안전 영역, Time: 1768 ms, Memory: 33340 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,48 +1,58 @@
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(100000)
 
-# 행렬크기, 행렬 입력받기
-size = int(input())
-matrix = []
+N = int(sys.stdin.readline())
+
+graph = []
 max_ = 0
-for i in range(size):
-    matrix.append(list(map(int,input().split())))
-    for j in range(size):
-        if matrix[i][j] > max_:
-            max_ = matrix[i][j]
 
-def dfs(x, y, height):
-    # 델타 이동 좌표
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
+def dfs(x : int, y : int, height : int):
 
-    for delta in range(4):
-        nx = x + dx[delta]
-        ny = y + dy[delta]
+    # 상하좌우 델타탐색
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
         
-        if (-1 < nx < size) and (-1 < ny < size):
-            if matrix[nx][ny] > height and not visited[nx][ny]:
+        # 그래프 범위 내이고 물에 잠기지 않으며 미방문일경우 재귀
+        if -1 < nx < N and -1 < ny < N:
+            if graph[nx][ny] > height and not visited[nx][ny]:
                 visited[nx][ny] = 1
                 dfs(nx, ny, height)
 
-answer = []
-# 높이는 1이상 max_미만의 정수(max_포함시 안전구역 없어짐)
+
+for i in range(N):
+    graph.append(list(map(int, sys.stdin.readline().strip().split())))
+
+    # 최대높이정보 갱신
+    for height in graph[i]:
+        if height > max_:
+            max_ = height
+
+safe_zone = []
+
+# 1부터 최대높이까지 탐색
 for height in range(1, max_):
-    # 카운트와 visited 행렬을 매번 초기화
-    count = 0
-    visited = [[0]*size for _ in range(size)]
     
-    for i in range(size):
-        for j in range(size):
-            if matrix[i][j] > height and not visited[i][j]:
+    # 매번 초기화
+    cnt = 0
+    visited = [[0] * N for _ in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            
+            # 미방문이고 높이가 현재 탐색 높이보다 높을 경우 재귀 / 물에 잠기지 않은 지역++ / 방문처리
+            if graph[i][j] > height and not visited[i][j]:
                 dfs(i, j, height)
                 visited[i][j] = 1
-                count += 1
-    answer.append(count)
+                cnt += 1
+
+    safe_zone.append(cnt)
     
-# 잠기는 영역 없을경우 안전지대 1개
-if sum(answer) == 0:
+# 물에 잠긴 지역이 없으면 1개의 지역
+if sum(safe_zone) == 0:
     print(1)
 else:
-    print(max(answer))
+    print(max(safe_zone))
