674302f46050753a66927361b547fdd71ac737f6
Kevin Lee <kklee0930@gmail.com>
2024-07-06 00:01:29 +0900

    [Silver V] Title: 단어 정렬, Time: 104 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,11 +1,12 @@
 import sys
 
-T = int(input())
-s = set()
-for tc in range(1, T+1):
-    str_input = str(sys.stdin.readline().split())
-    s.add(str_input)
+temp = set()
+for _ in range(int(sys.stdin.readline())):
+    str_ = sys.stdin.readline().strip()
+    temp.add(str_)
 
-sorted_set = sorted(s, key=lambda x: (len(x), x[::]))
-for word in sorted_set:
-    print(word.strip("['").strip("']"))
+temp = list(temp)
+result = sorted(temp, key = lambda x: (len(x), x))
+
+for li in result:
+    print(li)
