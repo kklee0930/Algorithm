93ad233622e2f4ad1e9d47f8e1deeedea4a6db05
Kevin Lee <kklee0930@gmail.com>
2024-07-05 19:55:57 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,17 +1,18 @@
-def Prime(num): # 소수 판별기
-    for i in range(2, int(num**0.5) + 1):
+import sys
+
+def is_prime(num):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
-
-
+        
 for _ in range(int(input())):
-    num = int(input())
-    a,b = num // 2, num // 2
+    n = int(sys.stdin.readline())
+    a, b = n // 2, n // 2
     while a > 0:
-        if Prime(a) == False or Prime(b) == False:
+        if not is_prime(a) or not is_prime(b):
             a -= 1
             b += 1
-        elif Prime(a) == True or Prime(b) == True:
+        elif is_prime(a) or is_prime(b):
             print(a, b)
             break
