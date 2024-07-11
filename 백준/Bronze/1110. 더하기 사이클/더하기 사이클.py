5fc98c1ea2f354fddd1573865d6e902d4ff663ba
Kevin Lee <kklee0930@gmail.com>
2024-07-11 10:18:14 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,14 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-count = 0
 num = n
+cnt = 0
 
 while True:
+    # 몫, 나머지, 몫+나머지 의 나머지 구해서 계산
     a = num // 10
     b = num % 10
     c = (a + b) % 10
     
-    num = (10 * b) + c
-    count += 1
+    num = 10 * b + c
+    cnt += 1
     if num == n:
         break
-print(count)
+print(cnt)
