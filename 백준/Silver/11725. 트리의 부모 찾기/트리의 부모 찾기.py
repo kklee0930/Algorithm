59fb42b444b5d0bead1734e3d187afd048d5c6f2
Kevin Lee <kklee0930@gmail.com>
2024-07-12 22:21:45 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 316 ms, Memory: 56888 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -11,24 +11,27 @@ for _ in range(n-1):
     tree[a].append(b)
     tree[b].append(a)
 
+# 각 노드의 부모 노드 저장하는 dictionary
 parent_dict = {}
 
-def bfs(node: int, parent: int):
-    visited = [0] * (n+1)
+def bfs(node: int):
+    visited = [0] * (n+1) # 방문여부
     queue = deque([])
-    queue.append((node, parent))
+    queue.append(node) # 노드 자기자신, 해당 노드의 부모 노드
     
     while queue:
-        curr_node, parent_node = queue.popleft()
-        adj_nodes = tree[curr_node]
+        curr_node = queue.popleft()
+        adj_nodes = tree[curr_node] # 현재 노드와 인접한 노드들
         
+        # 인접 노드 iteration 통해 탐색
         for num in adj_nodes:
+            # 루트에서 탐색을 시작하기 때문에 현재 노드가 부모 노드가 됨
             if not visited[num]:
                 visited[num] = 1
                 parent_dict[num] = curr_node
-                queue.append((num, curr_node))
+                queue.append(num)
     return
-  
-bfs(1, 1)
+
+bfs(1)
 for i in range(2, n+1):
     print(parent_dict[i])
