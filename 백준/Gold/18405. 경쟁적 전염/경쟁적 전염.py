f5bda10ce03e4ade80cce3acc8e6adfbb14aad3e
Kevin Lee <kklee0930@gmail.com>
2024-07-18 15:32:26 +0900

    [Gold V] Title: 경쟁적 전염, Time: 164 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -9,48 +9,38 @@ N, K = map(int, input().strip().split())
 graph = [list(map(int, input().strip().split())) for _ in range(N)]
 S, X, Y = map(int, input().strip().split()) # S초 뒤에 X,Y에 존재하는 바이러스
 
-def bfs(r: int, c: int, virus: int):
-    queue = deque([])
-    queue.append((r, c))
-    
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
-    
-    row, col = queue.popleft()
-    
-    # 상하좌우 탐색
-    for i in range(4):
-        nx = dx[i] + row
-        ny = dy[i] + col
-        
-        # 범위 체크
-        if 0 <= nx < N and 0 <= ny < N:
-            # 아직 감염되지 않았으면 감염처리
-            if not graph[nx][ny]:
-                graph[nx][ny] = virus
-                visited[nx][ny] = 1
-    return
+temp_list = []
+
+# 바이러스의 좌표들을 모두 추가
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] != 0:
+            temp_list.append((graph[i][j], i, j))
+            
+virus_list = deque(sorted(temp_list))
+
+time = 0
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 # 시간 S초까지 iteration
-for time in range(S):
+while time < S:
+    
     # 바이러스 1번부터 K번까지 iteration
-    for i in range(1, K+1):
-        # 전체 배열 iteration
-        empty_space = 0 # 아직 감염시킬 공간이 남아있는지 체크
-        visited = [[0] * (N) for _ in range(N)]
-        for j in range(N):
-            for k in range(N):
-                # 해당 좌표에 바이러스 i가 있으면 bfs
-                if graph[j][k] == i and not visited[j][k]:
-                    bfs(j, k, i)
-                    
-                elif not graph[j][k]:
-                    empty_space += 1
-            
-        if empty_space == 0: # 더 감염시킬 공간 없으면 바로 답 출력 후 종료
-            answer = 0 if not graph[X-1][Y-1] else graph[X-1][Y-1]
-            print(answer)
-            sys.exit(0)
+    for i in range(len(virus_list)):
         
-answer = 0 if not graph[X-1][Y-1] else graph[X-1][Y-1]
-print(answer)
+        virus_num, row, col = virus_list.popleft()
+        
+        # 상하좌우 탐색
+        for j in range(4):
+            nx = dx[j] + row
+            ny = dy[j] + col
+            
+            if 0 <= nx < N and 0 <= ny < N:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = virus_num
+                    virus_list.append((virus_num, nx, ny))
+    time += 1
+    
+print(graph[X-1][Y-1])
