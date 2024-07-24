e6d4bdebe5a43baba40d339269a309fb76835bab
Kevin Lee <kklee0930@gmail.com>
2024-07-24 22:47:48 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,12 +1,15 @@
-n = int(input())
+import sys
 
-num = list(map(int, input().split()))
-    
-dp = [0] * n
+input = sys.stdin.readline
 
-for i in range(n): # 0,1,2,3,4,5
-    for j in range(i): # 0,1,2,3,4
-        if num[i] > num[j] and dp[i] < dp[j]:
+N = int(input().strip())
+A = list(map(int, input().strip().split()))
+dp = [0] * N
+
+for i in range(N):
+    for j in range(i):
+        # 현재 값이 이전 값보다 크고 현재 누적 길이가 이전 누적 길이보다 더 작을 경우 갱신
+        if A[i] > A[j] and dp[i] < dp[j]:
             dp[i] = dp[j]
-    dp[i] += 1
+    dp[i] += 1 # i 인덱스를 수열의 첫 번째로 지정하는 경우를 가정
 print(max(dp))
