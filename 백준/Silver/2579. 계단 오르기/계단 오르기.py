11c085df596c97286fad2957d57aeb2b3fd49dd3
Kevin Lee <kklee0930@gmail.com>
2024-07-25 10:26:16 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,15 +1,20 @@
-num = int(input())
-stairs = [0 for _ in range(301)]
-dp = [0 for _ in range(301)]
+import sys
 
-for i in range(num):
-    stairs[i] = int(input())
+input = sys.stdin.readline
+
+stair_cnt = int(input().strip())
+stairs = [0 for _ in range(301)]
+dp = [0 for _ in range(301)] # 계단 별 최대 점수 값
+
+for i in range(stair_cnt):
+    stairs[i] = int(input().strip())
 
 dp[0] = stairs[0]
 dp[1] = stairs[0] + stairs[1]
-dp[2] = max(stairs[1]+stairs[2], stairs[0]+stairs[2])
+dp[2] = max(stairs[1] + stairs[2], stairs[0] + stairs[2])
 
-# 마지막 계단의 전 계단을 밟는 경우와 밟지 않는 경우
-for i in range(3, num):
-    dp[i] = max(dp[i-3]+stairs[i-1]+stairs[i], dp[i-2]+stairs[i])
-print(dp[num-1])
+# n-1 계단 밟기 vs n-2 계단 밟기
+for i in range(3, stair_cnt):
+    dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i])
+    
+print(dp[stair_cnt-1])
