28c1eaca6e2fc73839b5d442ecaf200dde34715e
Kevin Lee <kklee0930@gmail.com>
2025-04-24 00:30:20 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 15908 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,14 +1,23 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
-        sc.close();
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        bw.write((a+b) + "\n");
+        bw.write((a-b) + "\n");
+        bw.write((a*b) + "\n");
+        bw.write((a/b) + "\n");
+        bw.write((a%b) + "\n");
+        
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
